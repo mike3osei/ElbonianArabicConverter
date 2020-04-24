3fc613885291097940fad15d283341f28a1f910c
Robear Mankaryous <rmankaryous12@gmail.com>
2020-04-24 18:56:00 -0400

    done?

--- src/converter/ElbonianArabicConverter.java
+++ src/converter/ElbonianArabicConverter.java
@@ -3,6 +3,9 @@ package converter;
 import converter.exceptions.MalformedNumberException;
 import converter.exceptions.ValueOutOfBoundsException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * This class implements a converter that takes a string that represents a number in either the
  * Elbonian or Arabic numeral form. This class has methods that will return a value in the chosen form.
@@ -13,6 +16,12 @@ public class ElbonianArabicConverter {
 
     // A string that holds the number (Elbonian or Arabic) you would like to convert
     private final String number;
+    ArrayList<Character> validCharacters = new ArrayList<>();
+    ArrayList<Character> singleCharacters = new ArrayList<>();
+    ArrayList<Character> doubleCharacters = new ArrayList<>();
+    HashMap<Character, Integer> characterValues = new HashMap<>();
+    HashMap<Character, Integer> pointsRemaining = new HashMap<>();
+    private Type type;
 
     /**
      * Constructor for the ElbonianArabic class that takes a string. The string should contain a valid
@@ -22,15 +31,155 @@ public class ElbonianArabicConverter {
      * number is Elbonian, it must be a valid Elbonian representation of a number.
      *
      * @param number A string that represents either a Elbonian or Arabic number.
-     * @throws MalformedNumberException Thrown if the value is an Elbonian number that does not conform
-     * to the rules of the Elbonian number system. Leading and trailing spaces should not throw an error.
+     * @throws MalformedNumberException  Thrown if the value is an Elbonian number that does not conform
+     *                                   to the rules of the Elbonian number system. Leading and trailing spaces should not throw an error.
      * @throws ValueOutOfBoundsException Thrown if the value is an Arabic number that cannot be represented
-     * in the Elbonian number system.
+     *                                   in the Elbonian number system.
      */
     public ElbonianArabicConverter(String number) throws MalformedNumberException, ValueOutOfBoundsException {
-
+        fillWithValidElbonian(validCharacters);
+        fillWithSingleCharacters(singleCharacters);
+        fillWithDoubleCharacters(doubleCharacters);
+        fillWithCharacterValues(characterValues);
+        fillWithPointsRemaining(pointsRemaining);
         // TODO check to see if the number is valid, then set it equal to the string
-        this.number = number;
+        // Check if the string is a valid Arabic numeral
+        String trimmedString = number.trim();
+        if(trimmedString.length() > 0) {
+            if (checkArabic(trimmedString)) {
+                this.type = Type.ARABIC;
+            } else {
+                checkElbonian(trimmedString);
+                this.type = Type.ELBONIAN;
+            }
+            this.number = trimmedString;
+        } else {
+            throw new MalformedNumberException("String was empty or had no content");
+        }
+    }
+
+    private void fillWithPointsRemaining(HashMap<Character, Integer> array) {
+        array.put('M', 2000);
+        array.put('C', 200);
+        array.put('D', 300);
+        array.put('E', 600);
+        array.put('X', 20);
+        array.put('Y', 30);
+        array.put('Z', 60);
+        array.put('I', 2);
+        array.put('J', 3);
+        array.put('K', 6);
+    }
+
+    private void fillWithCharacterValues(HashMap<Character, Integer> array) {
+        array.put('M', 1000);
+        array.put('C', 100);
+        array.put('D', 300);
+        array.put('E', 600);
+        array.put('X', 10);
+        array.put('Y', 30);
+        array.put('Z', 60);
+        array.put('I', 1);
+        array.put('J', 3);
+        array.put('K', 6);
+    }
+
+    private boolean checkArabic(String string) throws ValueOutOfBoundsException {
+        try {
+            int arabicRepresentation = Integer.parseInt(string);
+            if (arabicRepresentation < 1 || arabicRepresentation > 2999) {
+                throw new ValueOutOfBoundsException("Arabic numeral was out of Elbonian number bounds");
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void checkElbonian(String string) throws MalformedNumberException {
+        if (!inOrderAndLegalCharacters(string) || !areLegalCount(string) || !areLegalPair(string)) {
+            throw new MalformedNumberException("Elbonian number does not conform to the rules of the Elbonian number system");
+        }
+    }
+
+    private boolean inOrderAndLegalCharacters(String string) {
+        int lastIndex = -1;
+        for (char c : string.toCharArray()) {
+            if (validCharacters.contains(c)) {
+                // Was a valid Elbonian character
+                int currentIndex = validCharacters.indexOf(c);
+                if (lastIndex == -1) {
+                    // Very first character
+                    // Set lastIndex to our first characters index
+                    lastIndex = currentIndex;
+                } else if (currentIndex > lastIndex) {
+                    // Our current characters index was larger than our previous
+                    return false;
+                } else {
+                    lastIndex = currentIndex;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean areLegalCount(String string) {
+        for (char c : doubleCharacters) {
+            int count = string.length() - string.replace(String.valueOf(c), "").length();
+            if (count > 2) {
+                return false;
+            }
+        }
+        for (char c : singleCharacters) {
+            int count = string.length() - string.replace(String.valueOf(c), "").length();
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean areLegalPair(String string) {
+        if (string.contains("D") && string.contains("E") && string.contains("C")) {
+            return false;
+        } else if (string.contains("Y") && string.contains("Z") && string.contains("X")) {
+            return false;
+        } else if (string.contains("J") && string.contains("K") && string.contains("I")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void fillWithDoubleCharacters(ArrayList<Character> array) {
+        array.add('M');
+        array.add('C');
+        array.add('X');
+        array.add('I');
+    }
+
+    private void fillWithValidElbonian(ArrayList<Character> array) {
+        array.add('I');
+        array.add('J');
+        array.add('K');
+        array.add('X');
+        array.add('Y');
+        array.add('Z');
+        array.add('C');
+        array.add('D');
+        array.add('E');
+        array.add('M');
+    }
+
+    private void fillWithSingleCharacters(ArrayList<Character> array) {
+        array.add('D');
+        array.add('E');
+        array.add('Y');
+        array.add('Z');
+        array.add('J');
+        array.add('K');
     }
 
     /**
@@ -41,7 +190,16 @@ public class ElbonianArabicConverter {
      */
     public int toArabic() {
         // TODO Fill in the method's body
-        return 1;
+        if (type == Type.ARABIC) {
+            return Integer.parseInt(number);
+        } else {
+            // Need to convert Elbonian number to arabic
+            int sum = 0;
+            for (char c : number.toCharArray()) {
+                sum += characterValues.get(c);
+            }
+            return sum;
+        }
     }
 
     /**
@@ -51,7 +209,61 @@ public class ElbonianArabicConverter {
      */
     public String toElbonian() {
         // TODO Fill in the method's body
-        return "I";
+        if (type == Type.ELBONIAN) {
+            return number;
+        } else {
+            // Need to convert Arabic number to Elbonian
+            int arabicRepresentation = Integer.parseInt(number);
+            StringBuilder elbonianRepresentation = new StringBuilder();
+            while (arabicRepresentation > 0) {
+                if (arabicRepresentation >= 1000 && pointsRemaining.get('M') > 0) {
+                    arabicRepresentation -= 1000;
+                    pointsRemaining.put('M', pointsRemaining.get('M') - 1000);
+                    elbonianRepresentation.append('M');
+                } else if (arabicRepresentation >= 600 && pointsRemaining.get('E') > 0) {
+                    arabicRepresentation -= 600;
+                    pointsRemaining.put('E', pointsRemaining.get('E') - 600);
+                    elbonianRepresentation.append('E');
+                } else if (arabicRepresentation >= 300 && pointsRemaining.get('D') > 0) {
+                    arabicRepresentation -= 300;
+                    pointsRemaining.put('D', pointsRemaining.get('D') - 300);
+                    elbonianRepresentation.append('D');
+                } else if (arabicRepresentation >= 100 && pointsRemaining.get('C') > 0) {
+                    arabicRepresentation -= 100;
+                    pointsRemaining.put('C', pointsRemaining.get('C') - 100);
+                    elbonianRepresentation.append('C');
+                } else if (arabicRepresentation >= 60 && pointsRemaining.get('Z') > 0) {
+                    arabicRepresentation -= 60;
+                    pointsRemaining.put('Z', pointsRemaining.get('Z') - 60);
+                    elbonianRepresentation.append('Z');
+                } else if (arabicRepresentation >= 30 && pointsRemaining.get('Y') > 0) {
+                    arabicRepresentation -= 30;
+                    pointsRemaining.put('Y', pointsRemaining.get('Y') - 30);
+                    elbonianRepresentation.append('Y');
+                } else if (arabicRepresentation >= 10 && pointsRemaining.get('X') > 0) {
+                    arabicRepresentation -= 10;
+                    pointsRemaining.put('X', pointsRemaining.get('X') - 10);
+                    elbonianRepresentation.append('X');
+                } else if (arabicRepresentation >= 6 && pointsRemaining.get('K') > 0) {
+                    arabicRepresentation -= 6;
+                    pointsRemaining.put('K', pointsRemaining.get('K') - 6);
+                    elbonianRepresentation.append('K');
+                } else if (arabicRepresentation >= 3 && pointsRemaining.get('J') > 0) {
+                    arabicRepresentation -= 3;
+                    pointsRemaining.put('J', pointsRemaining.get('J') - 3);
+                    elbonianRepresentation.append('J');
+                } else {
+                    arabicRepresentation -= 1;
+                    pointsRemaining.put('I', pointsRemaining.get('I') - 1);
+                    elbonianRepresentation.append('I');
+                }
+            }
+            return elbonianRepresentation.toString();
+        }
     }
 
+    private enum Type {
+        ARABIC,
+        ELBONIAN
+    }
 }
--- src/converter/tests/ConverterTests.java
+++ src/converter/tests/ConverterTests.java
@@ -5,7 +5,10 @@ import converter.exceptions.MalformedNumberException;
 import converter.exceptions.ValueOutOfBoundsException;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test cases for the ElbonianArabicConverter class.
@@ -35,4 +38,152 @@ public class ConverterTests {
     }
 
     // TODO Add more test cases
+    @Test
+    public void ArabicToElbonianGivenTestElbonian() throws MalformedNumberException, ValueOutOfBoundsException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("MMDZYJI");
+        assertEquals(converter.toArabic(), 2394);
+    }
+
+    @Test
+    public void ArabicToElbonianGivenTestArabic() throws MalformedNumberException, ValueOutOfBoundsException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("2394");
+        assertEquals(converter.toElbonian(), "MMDZYJI");
+    }
+
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void negativeNumberTest() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("-1");
+    }
+
+    @Test
+    public void checkAll() throws ValueOutOfBoundsException, MalformedNumberException {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        // Check every possible arabic value
+        for(int i = 1; i < 2999; i++) {
+            ElbonianArabicConverter converter = new ElbonianArabicConverter(i + "");
+            hashMap.put(converter.toElbonian(), i);
+            assertEquals(converter.toArabic(), i);
+        }
+        // Check every possible elbonian value
+        for (String string : hashMap.keySet()) {
+            ElbonianArabicConverter converter = new ElbonianArabicConverter(string);
+            int value = hashMap.get(string);
+            assertEquals(value, converter.toArabic());
+        }
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkLowercase() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("mmdzyji");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkOrderMalformed() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("MMCD");
+    }
+
+    @Test
+    public void checkOrder() throws ValueOutOfBoundsException, MalformedNumberException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("MMDC");
+        assertEquals(converter.toArabic(), 2400);
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkPairingMalformed() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("MMEDC");
+    }
+
+    @Test
+    public void checkPairing() throws ValueOutOfBoundsException, MalformedNumberException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("MMEDZYKJ");
+        assertEquals(converter.toArabic(), 2999);
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkCountMalformed() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("MMM");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkCountMalformed2() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("MMDD");
+    }
+
+    @Test
+    public void checkBottomBound() throws ValueOutOfBoundsException, MalformedNumberException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("1");
+        assertEquals(converter.toElbonian(), "I");
+    }
+
+    @Test
+    public void checkTopBound() throws ValueOutOfBoundsException, MalformedNumberException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("2999");
+        assertEquals(converter.toElbonian(), "MMEDZYKJ");
+    }
+
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void checkOverTopBound() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("3000");
+    }
+
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void checkUnderTopBound() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("0");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkEmptyString() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkSpaceString() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter(" ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkSpaceBetween() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("9 9");
+    }
+
+    @Test
+    public void checkTrim() throws ValueOutOfBoundsException, MalformedNumberException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter(" 99 ");
+        assertEquals(converter.toElbonian(), "ZYKJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkTrimAndSpaceBetween() throws ValueOutOfBoundsException, MalformedNumberException{
+        new ElbonianArabicConverter(" 9 9 ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkSmileEmoji() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("😃");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkDecimal() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter("12.5");
+    }
+
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void checkMaxValue() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter(Integer.MAX_VALUE + "");
+    }
+
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void checkMinValue() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter(Integer.MIN_VALUE + "");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkMaxDouble() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter(Double.MAX_VALUE + "");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void checkMinDouble() throws ValueOutOfBoundsException, MalformedNumberException {
+        new ElbonianArabicConverter(Double.MIN_VALUE + "");
+    }
 }
